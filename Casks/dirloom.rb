8e0364972e633e094b1082acff193de21798b0d7
Samuel Benny-hynn <icelltech.dev@gmail.com>
2026-08-21 11:33:57 +0100

    fix(cask): use Homebrew completion generator

--- Casks/dirloom.rb
+++ Casks/dirloom.rb
@@ -27,27 +27,9 @@ cask "dirloom" do
   end
 
   binary "dirloom"
-
-  postflight do
-    executable = staged_path/"dirloom"
-    prefix = Pathname.new(HOMEBREW_PREFIX)
-    begin
-      bash_dir = prefix/"etc/bash_completion.d"
-      zsh_dir = prefix/"share/zsh/site-functions"
-      fish_dir = prefix/"share/fish/vendor_completions.d"
-      pwsh_dir = prefix/"share/pwsh/completions"
-      bash_dir.mkpath
-      zsh_dir.mkpath
-      fish_dir.mkpath
-      pwsh_dir.mkpath
-      (bash_dir/"dirloom").write system_command(executable, args: ["completion", "bash"]).stdout
-      (zsh_dir/"_dirloom").write system_command(executable, args: ["completion", "zsh"]).stdout
-      (fish_dir/"dirloom.fish").write system_command(executable, args: ["completion", "fish"]).stdout
-      (pwsh_dir/"dirloom.ps1").write system_command(executable, args: ["completion", "powershell"]).stdout
-    rescue StandardError => e
-      puts "Could not install generated shell completions (#{e.message}); run dirloom completion <shell> manually."
-    end
-  end
+  generate_completions_from_executable "dirloom",
+                                       shell_parameter_format: :cobra,
+                                       shells:                 [:bash, :zsh, :fish, :pwsh]
 
   caveats <<~EOS
     Generate shell completions from the installed binary if they were not
